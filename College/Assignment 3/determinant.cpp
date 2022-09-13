720eff6482f243035a30842d73f67911c71e3617
Andreas Notokuusmo <andreasnotokusumo23@gmail.com>
2022-09-13 11:37:37 +0700

    :sparkles: Determinant Update

--- College/Assignment 3/determinant.cpp
+++ College/Assignment 3/determinant.cpp
@@ -3,12 +3,34 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
   double a, b, c;
+  double x, y;
 
   cout << "Program Determinan" << endl;
   cin >> a >> b >> c;
 
-  double determinant = (pow(b, 2)) - (4 * a * c);
+  double det = (pow(b, 2)) - (4 * a * c);
+
+  if (det > 0)
+  {
+    x = (-b + sqrt(det)) / 2 * a;
+    y = (-b - sqrt(det)) / 2 * a;
+  }
+  else if (det == 0)
+  {
+    x = y = -b / 2 * a;
+  }
+  else if (det < 0)
+  {
+    x = -b / 2 * a + (sqrt(-det) / 2 * a);
+    y = -b / 2 * a - (sqrt(-det) / 2 * a);
+  }
+
+  cout << "Akar-akar dari persamaan:" << endl;
+  cout << "X: " << x << endl;
+  cout << "Y: " << y << endl;
+
   return 0;
 }
